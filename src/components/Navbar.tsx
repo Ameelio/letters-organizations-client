1d223f06efdb51db1276761b9c5a28bc752d57f7
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2020-07-11 19:35:09 -0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React, {FunctionComponent } from 'react';
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+
+const NavBar : FunctionComponent = () => {
+    return (
+        <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+        sticky="top"
+      >
+        <Navbar.Brand>
+          <Link to="/">
+            <img
+              src={require("../assets/logo.svg")}
+              width="150px"
+              className="d-inline-block align-top"
+              alt="Ameelio logo"
+            />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+        </Nav>
+      </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default NavBar;
---
+++ src/pages/Volunteers.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+
+
+const VolunteerPage:FunctionComponent = () => {
+    
+    return (
+        <div/>
+    );
+}
+
+export default VolunteerPage;
+
