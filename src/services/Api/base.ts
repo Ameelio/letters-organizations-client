a7954fedc63c24976e5f9705e565a08b1cb8a559
Sarah <sarah@zagaran.com>
2020-08-10 13:25:33 -0400

    add env variables

--- src/services/Api/base.ts
+++ src/services/Api/base.ts
@@ -1,1 +1,4 @@
-export const API_URL = 'http://127.0.0.1:8000/api/'; // TODO: Change to use env variable
+export const API_URL =
+  process.env && process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL
+    : '';
