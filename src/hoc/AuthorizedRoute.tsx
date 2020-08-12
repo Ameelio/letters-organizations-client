08c8a6d6b1894799fda479bdffbda153c1831432
Sarah <sarah@zagaran.com>
2020-08-12 12:32:10 -0400

    add back authorized route class oops

---
+++ src/hoc/AuthorizedRoute.tsx
@@ -0,0 +1,25 @@
+import { Route, Redirect, RouteProps } from 'react-router';
+import { connect } from 'react-redux';
+import { RootState } from '../redux';
+import React from 'react';
+
+interface Props extends RouteProps {
+  authenticated: boolean;
+}
+
+class AuthorizedRoute extends Route<Props> {
+  public render() {
+    if (!this.props.authenticated) {
+      const renderComponent = () => <Redirect to="/login" />;
+      return <Route {...this.props} component={renderComponent} />;
+    } else {
+      return <Route {...this.props} />;
+    }
+  }
+}
+
+const mapStateToProps = (state: RootState) => ({
+  authenticated: state.user.authInfo.isLoggedIn,
+});
+
+export default connect(mapStateToProps)(AuthorizedRoute);
